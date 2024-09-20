cc072a94979482e9a51e702f204d011e6244ad28
Christian Larrea <crlarrea@protonmail.com>
2024-09-20 20:32:27 +0200

    add tour page

---
+++ src/app/tour/page.jsx
@@ -0,0 +1,12 @@
+export default function Tour() {
+  return (
+    <section>
+      <article>
+        <p>tour</p>
+      </article>
+      <article>
+        <p>info</p>
+      </article>
+    </section>
+  );
+}
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <nav>
-        <p>header</p>
+        <ul>
+          <li>
+            <Link href="/">home</Link>
+          </li>
+          <li>
+            <Link href="/tour">tour</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
